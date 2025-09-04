e70b0d7eed43af02acc24b8606b25e6d021715be
JhonyOwner <jvgbrisola@gmail.com>
2025-09-04 05:18:20 -0300

    Adicionado e minificado o main.js pelo uglify

--- gruntfile.js
+++ gruntfile.js
@@ -97,7 +97,8 @@ module.exports = function (grunt) {
     grunt.loadNpmTasks('grunt-replace');
     grunt.loadNpmTasks('grunt-contrib-htmlmin');
     grunt.loadNpmTasks('grunt-contrib-clean');
+    grunt.loadNpmTasks('grunt-contrib-uglify');
 
     grunt.registerTask('default', ['watch']);
-    grunt.registerTask('build', ['less:production', 'htmlmin:dist', 'replace:dist', 'clean']);
+    grunt.registerTask('build', ['less:production', 'htmlmin:dist', 'replace:dist', 'clean', 'uglify']);
 }
